b2e505d0c427768b4ed4d9b1e316a8e916dee5ee
Uhiene <107491357+Uhiene@users.noreply.github.com>
2022-08-17 00:17:33 +0100

    Add files via upload

---
+++ easybank-landing-page-master/bank.js
@@ -0,0 +1,11 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".navbar");
+
+navToggle.addEventListener("click", function () {
+  if(links.classList.contains('show-links')){
+    links.classList.remove('show-links');
+  } else{
+    links.classList.add('show-links');
+  }
+  // links.classList.toggle("show-links");
+});
